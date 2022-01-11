107d762a70d3ce0ff6f84dca6405f1189ef05251
Nooratt <nooratt@gmail.com>
2022-01-11 13:07:00 +0200

    Add project files.

---
+++ TaskDudes/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TaskDudes.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Welcome to TaskMates";
+            NewTaskCommand = new Command(async () => await Browser.OpenAsync("https://fi.wikipedia.org/wiki/Kana"));
+        }
+
+        public ICommand NewTaskCommand { get; }
+    }
+}
---
+++ TaskDudes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TaskDudes.ViewModels;
+using Xamarin.Forms;
+
+namespace TaskDudes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
